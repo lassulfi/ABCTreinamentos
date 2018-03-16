9557d1115d0877a5c60a42c866bbfaff2bcb9b0a
Luis Daniel Assulfi <lassulfi@gmail.com>
2018-03-16 13:28:58 -0300

    Add files via upload

---
+++ src/br/Mec.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br;
+
+import br.abctreinamentos.Universidade;
+import static br.abctreinamentos.Universidade.*;
+
+/**
+ *
+ * @author luis.assulfi
+ */
+public class Mec {
+    
+    void avaliarEnsino(){
+        Universidade ufpa = new Universidade();
+        
+    }
+    
+    public static void main(String[] args) {
+        gerarRelatorioProfessoresMestresDoutores();
+    }
+    
+}
